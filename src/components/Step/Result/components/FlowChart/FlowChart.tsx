3dec410fb67196c6119b360eb06dba32f6a79191
Panduss <zsuzsi.hudacsko@gmail.com>
2022-05-12 23:22:18 +0200

    rendering nodes dynamically based on their height

--- src/components/Step/Result/components/FlowChart/FlowChart.tsx
+++ src/components/Step/Result/components/FlowChart/FlowChart.tsx
@@ -1,68 +1,67 @@
-import ReactFlow, {Edge, Node, Position, ReactFlowProvider} from 'react-flow-renderer';
-import React, {useEffect, useMemo, useState} from 'react';
-import {IOption, IOptionSummary, IProperty} from "../../../../../store/flow/flowTypes";
+import React, {useCallback, useEffect, useLayoutEffect, useState} from "react";
+import ReactFlow, {
+    ReactFlowProvider,
+    Node,
+    applyNodeChanges,
+    useNodesState, useEdgesState
+} from "react-flow-renderer";
 import PropertyNodeType from "../NodeTypes/PropertyNodeType";
 import OptionNodeType from "../NodeTypes/OptionNodeType";
 import WinnerNodeType from "../NodeTypes/WinnerNodeType";
+import {getLayoutedElements} from "../../../../../utils/getLayoutedElements";
+import {useAppSelector} from "../../../../../store/store";
+import {IOption, IOptionSummary} from "../../../../../store/flow/flowTypes";
+import {winnerCalculation} from "../../../../../utils/calculations";
+import {getNodes} from "../../../../../utils/getNodes";
+import {getEdges} from "../../../../../utils/getEdges";
+import sharedStyles from "../../../../../styles/shared.module.scss";
+import CustomMotionDiv from "../../../../CustomMotionDiv/CustomMotionDiv";
 
-interface FlowChartProps {
-    properties: IProperty[],
-    summary: IOptionSummary[],
-    normalisedData: IOption[],
-    winner: IOptionSummary[]
-}
+const nodeTypes = {
+    property: PropertyNodeType,
+    option: OptionNodeType,
+    winner: WinnerNodeType
+};
 
-const FlowChart = ({properties, summary, normalisedData, winner}: FlowChartProps) => {
+const nodeHasDimension = (el: Node) => el.width && el.height;
 
-    const [nodes, setNodes] = useState<Node[]>([]);
-    const [edges, setEdges] = useState<Edge[]>([]);
-    const [nodeIds, setNodeIds] = useState<{property: string[], option: string[], winner: string[]}>();
+const FlowChart = () => {
+    const [nodes, setNodes, onNodeChange] = useNodesState([]);
+    const [edges, setEdges] = useEdgesState([]);
+    const [shouldLayout, setShouldLayout] = useState(true);
+    const flow = useAppSelector((state) => state.flow.current);
 
-    const nodeTypes = useMemo(() => ({
-        property: PropertyNodeType,
-        option: OptionNodeType,
-        winner: WinnerNodeType
-    }), []);
+    const [result, setResult] = useState<{
+        normalisedData: IOption[],
+        summary: IOptionSummary[],
+        winner: IOptionSummary[]
+    }>()
 
     useEffect(() => {
-        let propertyXPosition = -220;
-        const propertyNodes = properties.map((item) => {
-            propertyXPosition = propertyXPosition + 240;
+        /*
+            Calculate results
+         */
+        if (!flow) return;
+        const data = winnerCalculation(flow);
+        setResult(() => data);
+    }, [flow])
+
+    useEffect(() => {
+        /*
+            Get nodes and edges
+         */
+        if (!flow || !result || !result.normalisedData || !result.summary || !result.winner) return;
+
+        const updateOptions = result.normalisedData.map(item => {
             return {
-                id: `property-${item.id}`,
-                type: 'property',
-                data: item,
-                position: {x: propertyXPosition, y: 0},
-                sourcePosition: Position.Bottom
+                ...item,
+                score: result.summary.find(summary => summary.id === item.id)?.valueSum
             }
-        });
-        let optionXPosition = -220;
-        const optionNodes = normalisedData.map((item) => {
-            optionXPosition = optionXPosition + 240;
-            const data = {
-                option: item,
-                finalScore: summary.find(summary => summary.id === item.id)?.valueSum
-            }
-            return {
-                id: `option-${item.id}`,
-                type: 'option',
-                data: data,
-                position: {x: optionXPosition, y: 250},
-                targetPosition: Position.Top,
-                sourcePosition: Position.Bottom
-            }
-        });
-        let winnerXPosition = -220;
-        const winnerNodes = winner.map((item) => {
-            winnerXPosition = winnerXPosition + 240;
-            return {
-                id: `winner-${item.id}`,
-                type: 'winner',
-                data: item,
-                position: {x: winnerXPosition, y: 700},
-                targetPosition: Position.Top
-            }
-        });
+        })
+
+        const propertyNodes = getNodes(flow.properties, 'property');
+        const optionNodes = getNodes(updateOptions, 'option');
+        const winnerNodes = getNodes(result.winner, 'winner');
 
         const ids = {
             property: propertyNodes.map(item => item.id),
@@ -70,51 +69,41 @@ const FlowChart = ({properties, summary, normalisedData, winner}: FlowChartProps
             winner: winnerNodes.map(item => item.id)
         }
 
-        setNodeIds(() => ids);
-        setNodes(() => [...propertyNodes, ...optionNodes, ...winnerNodes]);
-    }, [winner, summary, properties, normalisedData])
+        const propertyToOptionsEdges = getEdges(ids.property, ids.option);
+        const optionsToWinnersEdges = getEdges(ids.option, ids.winner);
+
+        const nodes = [...propertyNodes, ...optionNodes, ...winnerNodes];
+        const edges = [...propertyToOptionsEdges.flat(), ...optionsToWinnersEdges.flat()]
+
+        setNodes(() => nodes);
+        setEdges(() => edges);
+        setShouldLayout(() => true);
+    }, [result])
+
+    useLayoutEffect(() => {
+        if (shouldLayout && nodes && nodes.length && nodes.length > 0 && nodes.every(nodeHasDimension)) {
+            const elementsWithLayout = getLayoutedElements({nodes, edges});
+            setNodes(elementsWithLayout.nodes)
+            setShouldLayout(() => false);
+        }
+    }, [nodes]);
 
     useEffect(() => {
-        if (!nodeIds) return;
-        const propertyToOptionsEdges = nodeIds.property.map(propertyId => {
-            return nodeIds.option.map(optionId => {
-                return {
-                    id: `${propertyId}-${optionId}-target`,
-                    source: propertyId,
-                    target: optionId,
-                    sourceHandle: propertyId,
-                    targetHandle: optionId,
-                    style: { stroke: '#fff' }
-                }
-            })
-        }).flat();
-
-        const optionsToWinnersEdges = nodeIds.option.map(optionId => {
-            return nodeIds.winner.map(winnerId => {
-                return {
-                    id: `${optionId}-${winnerId}-source`,
-                    source: optionId,
-                    target: winnerId,
-                    sourceHandle: optionId,
-                    targetHandle: winnerId,
-                    style: { stroke: '#fff' }
-                }
-            })
-        }).flat()
-
-        setEdges(() => [...propertyToOptionsEdges.flat(), ...optionsToWinnersEdges.flat()])
-    }, [nodeIds])
+        console.log('shouldLayout', shouldLayout)
+    }, [shouldLayout])
 
     return (
-        <ReactFlowProvider>
-            <ReactFlow
-                nodeTypes={nodeTypes}
-                nodes={nodes}
-                edges={edges}
-            />
-        </ReactFlowProvider>
+        <CustomMotionDiv className={sharedStyles.page}>
+            <ReactFlowProvider>
+                <ReactFlow nodes={nodes} edges={edges} nodeTypes={nodeTypes} onNodesChange={onNodeChange}/>
+            </ReactFlowProvider>
+        </CustomMotionDiv>
     )
-
-}
+};
 
 export default FlowChart;
+
+
+
+
+
